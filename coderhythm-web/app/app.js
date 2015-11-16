3b4f79e6331c87213509b716adc4d23f6dfde5ce
Mark Doucette <mark@rhythmtech.ca>
2015-11-16 13:09:31 -0800

    Remove login and it's references for now

--- coderhythm-web/app/app.js
+++ coderhythm-web/app/app.js
@@ -2,7 +2,7 @@
 
 // Defining Angular app model with all other dependent modules
 var mostPopularListingsApp = angular.module('mostPopularListingsApp',['ngRoute',
-	'mostPopularListingsApp.home','mostPopularListingsApp.about','mostPopularListingsApp.login']);
+	'mostPopularListingsApp.home','mostPopularListingsApp.about']);
 
 mostPopularListingsApp.config(function($routeProvider, $locationProvider, $httpProvider) {
 	

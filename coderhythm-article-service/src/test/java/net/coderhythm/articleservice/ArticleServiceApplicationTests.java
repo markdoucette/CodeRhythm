7699ed15313d6618ef0b26534ed8aae4475b4261
Mark Doucette <mark@rhythmtech.ca>
2015-11-18 10:52:10 -0800

    Initialize ArticleService module

---
+++ coderhythm-article-service/src/test/java/net/coderhythm/articleservice/ArticleServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package net.coderhythm.articleservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ArticleServiceApplication.class)
+public class ArticleServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
